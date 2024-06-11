b85ea7778f035ae711a75b45e46928964d36d8a3
matias <xgalladex1234@gmail.com>
2024-06-11 00:20:14 -0400

    areglado el 3

--- ejercicios/3.js
+++ ejercicios/3.js
@@ -1,32 +1,15 @@
 function longestSubstring(s) {
     let contador = 1;
     let max = 1;
-    let indiceinicial = 0;
-    let indicecomparar = 1;
-    const longitud = s.length;
-    
-    for (let i = indiceinicial; i <= indicecomparar; i++) {
-        if (longitud === 0) {
-            return 0;
-        }
-        if (s[i] !== s[indicecomparar]) {
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === s[i + 1]) {
             contador++;
         } else {
-            i = indiceinicial;
-            indiceinicial++;
             contador = 1;
-            indicecomparar = indiceinicial + 1;
         }
-        if (indicecomparar === longitud) {
-            break;
-        }
-        if (i + 1 === indicecomparar) {
-            i = indiceinicial - 1;
-            indicecomparar++;
-            if (contador > max) {
-                max = contador;
-            }
-            contador = 1;
+        if (contador > max) {
+            max = contador;
         }
     }
     return max;
